3f3bb3b62532e41372bcc3fa49c89c555375317e
Torsten Boettjer <57480261+hcops@users.noreply.github.com>
2024-07-22 12:26:25 +0200

    Update devenv.nix

--- devenv.nix
+++ devenv.nix
@@ -2,15 +2,12 @@
 
 {
   # https://devenv.sh/basics/
-  env.GREET = "devenv";
+  env.GREET = "sandbox";
 
   # https://devenv.sh/packages/
   packages = with pkgs; [
     postgresql_15
     cargo-pgrx
-    #graphqurl
-    #graphql-client
-    #nixos-generators
     # linter
     #sqlfluff   # SQL
     #shellcheck # Shell
@@ -40,36 +37,94 @@
 
   # https://devenv.sh/services/
   services.postgres = {
-    package = pkgs.postgresql_15.withPackages (p: [ p.age ]);
     enable = true;
+    package = pkgs.postgresql_15;
     settings.port = 5432;
     listen_addresses = "127.0.0.1";
+    initialDatabases = [{ name = "mydb"; }];
+    extensions = extensions: [
+      extensions.timescaledb
+    ];
+    settings.shared_preload_libraries = "timescaledb";
     initialScript = ''
-      CREATE DATABASE "mydb";
+      CREATE EXTENSION IF NOT EXISTS timescaledb;
     '';
   };
 
-  # pre-commit.hooks = {
-  #   shellcheck.enable = true;
-  #   mdsh.enable = true;
+  services.postgres = {
+  };
+
+  pre-commit.hooks = {
+    shellcheck.enable = true;
+    mdsh.enable = true;
     # SQL
-  #   sqllint = {
-  #     enable = true;
-  #     name = "SQL linter";
-  #     entry = "${pkgs.sqlfluff}/bin/sqlfluff lint --dialect postgres -e CP01,CP05,LT02 --nofail";
-  #     files = "\\.sql$";
-  #     pass_filenames = true;
-  #   };
-  # };
+    sqllint = {
+      enable = true;
+      name = "SQL linter";
+      entry = "${pkgs.sqlfluff}/bin/sqlfluff lint --dialect postgres -e CP01,CP05,LT02 --nofail";
+      files = "\\.sql$";
+      pass_filenames = true;
+    };
+  };
 
   # https://devenv.sh/languages/
-  # languages.nix.enable = true;
-
-  # https://devenv.sh/reference/options/#languagesrustchannel
+  # languages.ansible.enable = true;
+  # languages.c.enable = true;
+  # languages.clojure.enable = true;
+  # languages.cplusplus.enable = true;
+  # languages.crystal.enable = true;
+  # languages.cue.enable = true;
+  # languages.dart.enable = true;
+  # languages.deno.enable = true;
+  # languages.dotnet.enable = true;
+  # languages.elixir.enable = true;
+  # languages.elm.enable = true;
+  # languages.erlang.enable = true;
+  # languages.fortran.enable = true;
+  # languages.gawk.enable = true;
+  # languages.gleam.enable = true;
+  # languages.go.enable = true;
+  # languages.haskell.enable = true;
+  # languages.idris.enable = true;
+  # languages.java.enable = true;
+  # languages.javascript.enable = true;
+  # languages.jsonnet.enable = true;
+  # languages.julia.enable = true;
+  # languages.kotlin.enable = true;
+  # languages.lua.enable = true;
+  # languages.nim.enable = true;
+  languages.nix.enable = true;
+  # languages.ocaml.enable = true;
+  # languages.odin.enable = true;
+  # languages.opentofu.enable = true;
+  # languages.pascal.enable = true;
+  # languages.perl.enable = true;
+  # languages.php.enable = true;
+  # languages.purescript.enable = true;
+  # languages.python.enable = true;
+  # languages.python.version = "3.11.3";
+  # languages.r.enable = true;
+  # languages.racket.enable = true;
+  # languages.raku.enable = true;
+  # languages.robotframework.enable = true;
+  # languages.ruby.enable = true;
   languages.rust.enable = true;
+  languages.rust.channel = "stable";
+  # languages.scala.enable = true;
+  # languages.shell.enable = true;
+  # languages.solidity.enable = true;
+  # languages.standardml.enable = true;
+  # languages.swift.enable = true;
+  # languages.terraform.enable = true;
+  # languages.texlive.enable = true;
+  # languages.typescript.enable = true;
+  # languages.unison.enable = true;
+  # languages.v.enable = true;
+  # languages.vala.enable = true;
+  # languages.zig.enable = true;
 
   # https://devenv.sh/pre-commit-hooks/
-  # pre-commit.hooks.shellcheck.enable = true;
+  pre-commit.hooks.shellcheck.enable = true;
 
 
   # See full reference at https://devenv.sh/reference/options/
